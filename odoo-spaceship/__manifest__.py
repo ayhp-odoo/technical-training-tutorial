050c3b0eeaca97676e2089574257f237adad0d28
ayhp-odoo <ayhp@odoo.com>
2022-06-13 17:36:56 -0500

    [ADD] Added Spaceship travel module

---
+++ odoo-spaceship/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- Encoding: utf-8 -*-
+{
+  'name': 'Odoo Spaceship Travel',
+  'summary': '''App to organize the logistics''',
+  'description': '''
+    An module to plan and help Odoo get to Moon
+  ''',
+  'author': 'Odoo',
+  'website': 'https://www.odoo.com',
+  'category': 'Logistcs',
+  'version': '0.1',
+  'depends': ['base'],
+  'data': [],
+  'demo': [],
+}
---
+++ odoo-spaceship/models/spaceship.py
@@ -0,0 +1,7 @@
+# -*- Encoding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class SpaceShip(models.Model):
+  _name = "odoo.spaceship"
+  _description = "Spaceship info"
